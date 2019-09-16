d67257fbd7b3df3b0a74d1f09dbb3fbd2a5fa197
Lisa Han <ellesis@gmail.com>
2019-09-16 09:07:29 -0700

    add more data of customers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Customer from './components/customer'
 import './App.css';
 
-const customer = {
-  'id': 1,
-  'image': 'http://placeimg.com/64/64/people',
-  'name': 'Alex',
-  'birthday': '991222',
-  'gender': 'Meal',
-  'job' : 'Student'
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': 'Alex',
+    'birthday': '991222',
+    'gender': 'Male',
+    'job' : 'Student'
+  },{
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': 'Julia',
+    'birthday': '051222',
+    'gender': 'Female',
+    'job' : 'Student'
+  },{
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': 'Lisa',
+    'birthday': '791222',
+    'gender': 'Female',
+    'job' : 'Student'
+  }
+]
 
-function App() {
-  return (
-    <Customer
-      id={customer.id}
-      image={customer.image}
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
-  );
+class App  extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(item=>{
+            return (
+              <Customer
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                birthday={item.birthday}
+                gender={item.gender}
+                job={item.job}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
